bfff11bd35a7e0d844ed2db7f2bc0a75805e5322
Luis Rómulo Peche Aparcana <luis.pechea@cajaica.pe>
2026-04-14 18:09:09 -0500

    refactor(agent): :wastebasket: clean repository

--- src/llm/domain/llm_client.py
+++
@@ -1,9 +0,0 @@
-
-from abc import ABC, abstractmethod
-from langchain_core.runnables import Runnable
-from llm.domain.llm_entities import LlmConfig
-
-class LlmClient(ABC):
-    @abstractmethod
-    def llm_client(self,config :LlmConfig) -> Runnable:
-        pass
--- src/llm/domain/llm_service.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from langchain_core.runnables import Runnable
-from llm.domain.llm_entities import LLMRequestConfig
-
-class LlmService(ABC):
-    
-    @abstractmethod
-    async def set_llm_Service(llm_config : LLMRequestConfig) -> Runnable:
-        pass
